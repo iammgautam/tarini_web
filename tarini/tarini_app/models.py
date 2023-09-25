f73b35f16e67dd00484b6053f30fa99813d703c9
zengboi <mithileshgautam666@gmail.com>
2023-09-25 12:29:44 +0530

    Latest commit

--- tarini/tarini_app/models.py
+++ tarini/tarini_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=50,verbose_name="Full Name"), 
+    phone = models.CharField(max_length=10)
+    pet_breed = models.CharField(max_length=50)
+    reason = models.TextField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Booked Date")
+
+    def __str__(self):
+        return self.name - (self.pet_breed)
--- tarini/tarini_app/views.py
+++ tarini/tarini_app/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
-
+from twilio.rest import Client
+import os
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
+
+
+def sms_sent(request):
+    account_sid = 'ACb4812c29ad3ed5f0f563d82ce8045ab8'
+    auth_token = '537ab06fade696e6dd8e615abbe00f48'
+    client = Client(account_sid, auth_token)
+
+    full_name = request.GET.get("full_name")
+    phone_number = request.GET.get("phone_number")
+    pet = request.GET.get("pet_breed")
+    reason = request.GET.get("message")
+    body = (
+        "Booking From Website: "
+        + "\nName: "
+        + full_name
+        + "\nPhone Number: "
+        + phone_number
+        + "\nPet Breed: "
+        + pet
+        + "\nReason: "
+        + reason
+    )
+    message = client.messages.create(
+        body=body, from_="+18509983979", to="+919365027852"
+    )
+    return render(request, "contact.html")
